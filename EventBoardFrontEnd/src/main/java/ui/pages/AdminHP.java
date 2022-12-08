8fd1b65000bc58fcbbe49a3156d118b2f68eff92
MegaMuni <konidalalohith@gmail.com>
2022-12-08 01:35:57 -0500

    Test

--- EventBoardFrontEnd/src/main/java/ui/pages/AdminHP.java
+++ EventBoardFrontEnd/src/main/java/ui/pages/AdminHP.java
@@ -22,14 +22,14 @@ import ui.components.Associations.AssociationsFeedTableModel;
  *
  * @author shrut
  */
-public final class AdminHomePage extends javax.swing.JFrame {
+public final class AdminHP extends javax.swing.JFrame {
 
     /**
      * Creates new form HomePage
      */
     private String users;
     private JSONObject inputJSON;
-    public AdminHomePage(JSONObject input) {
+    public AdminHP(JSONObject input) {
         this.inputJSON = input;
         initComponents();
         active();
@@ -393,7 +393,7 @@ public final class AdminHomePage extends javax.swing.JFrame {
 
         //</editor-fold>        
 //        java.awt.EventQueue.invokeLater(() -> {
-//            new AdminHomePage().setVisible(true);
+//            new AdminHP().setVisible(true);
 //        });
     }
 
